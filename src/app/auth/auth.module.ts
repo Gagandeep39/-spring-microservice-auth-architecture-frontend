aaf713fcf6126061389da98d2e318353e5c60df1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-09 16:54:16 +0000

    Added auth module Added login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-
-
+import { AuthComponent } from './auth.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, AuthComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
