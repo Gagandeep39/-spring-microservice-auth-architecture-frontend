305f9f45b92f4a9d1aafebeece506f8be670df7f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-09 18:15:12 +0000

    Created jwt, event service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
+import { User } from '../models/user.model';
+import { EventService } from './event.service';
+import { JwtHelperService } from './jwt-helper.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  authServiceUrl = `${environment.protocol}${environment.applicationUrl}`;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private jwtHelper: JwtHelperService,
+    private eventServie: EventService
+  ) {}
+
+  login(formData) {
+    return this.http.post(`${this.authServiceUrl}/auth/login`, formData).pipe(
+      tap((user: User) => {
+        this.saveToSessionStorage(user);
+        this.eventServie.loggedInUser.next(user);
+      })
+    );
+  }
+
+  saveToSessionStorage(user: User) {
+    sessionStorage.setItem('user', JSON.stringify(user));
+  }
 }
